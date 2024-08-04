2eb1b6e8804d67deaf48fdf5513b739e6c652a79
ashrut1011 <ashrutarora@gmail.com>
2024-08-04 23:24:18 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button"
+import { ResizableHandle } from "./components/ui/resizable"
+
+function App() {
+
+
+  return (
+    <>
+      
+      <Button>Test Button</Button>
+      {/* <ResizableHandle></ResizableHandle> */}
+    </>
+  )
+}
+
+export default App
