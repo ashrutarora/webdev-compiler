65bfa4f4aad5aaa8947c7a9e017d087c3a9fb3a2
ashrut1011 <ashrutarora@gmail.com>
2024-08-06 01:41:01 +0530

    added language change and selection functionality

--- client/src/redux/slices/compilerSlice.ts
+++ client/src/redux/slices/compilerSlice.ts
@@ -1,11 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {}
+export interface CompilerSliceStateType {
+    html: string,
+    css: string
+    javascript: string
+    currentLanguage: "html" | "css" | "javascript";
+}
+
+const initialState: CompilerSliceStateType = {
+    html: "",
+    css: "",
+    javascript: "",
+    currentLanguage: "html"
+}
 
 const compilerSlice = createSlice({
     name: "compilerSlice",
     initialState,
-    reducers:{}
+    reducers: {
+        updateCurrentLanguage: (state, action: PayloadAction<CompilerSliceStateType["currentLanguage"]>) => {
+            state.currentLanguage = action.payload
+        }
+    }
 })
 
 export default compilerSlice.reducer
+export const { updateCurrentLanguage } = compilerSlice.actions
